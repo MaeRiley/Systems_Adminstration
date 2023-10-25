9a658b2d69d2def80e297ccea9e2991bd0ad6e0f
Riley <102691556+MaeRiley@users.noreply.github.com>
2023-10-24 19:52:03 -0500

    Update nextcloud.backup.sh
    updated the email notifications on the exit code checks

--- nextcloud.backup.sh
+++ nextcloud.backup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#Backup and backup management script for Nextcloud data and configuration directories
-
 #backup directory and the number of days to keep backups
-backup_dir="you-path-here/nextcloud_backups/"
+backup_dir="/nextcloud_backups/"
 days_to_keep=5
 
 # Function to prune backups older than a specified number of days
+# NOTE: This will stop working in 77 years from year 2023 when variable "Y" changes to "21"
 prune_old_backups() {
             find "$backup_dir" -maxdepth 1 -type d -name "20*" -ctime +$days_to_keep -exec rm -rf {} \;
     }
@@ -16,6 +15,8 @@ prune_old_backups() {
                 echo "Old backups have been pruned."
         else
                     echo "Error: Failed to prune old backups."
+                    echo "Nextcloud failed to prune old backups on $(hostname)" | mail -s "Error Notification" -a "From: example@example.com" example@example.com
+
                         exit 1
     fi
 
@@ -31,13 +32,14 @@ mkdir -p "$backup_dir"
 chown mae:mae "$backup_dir"
 
 # Define source directories
-appdata_source="your-path-here/nextcloud/appdata/"
-data_source="you-path-here/nextcloud/data/"
+appdata_source="/nextcloud/appdata/"
+data_source="/nextcloud/data/"
 
 # Use rsync to backup the appdata folder
 rsync -av "$appdata_source" "$backup_dir/appdata/"
 if [ $? -ne 0 ]; then
             echo "Error: Failed to backup appdata folder."
+            echo "Nextcloud appdata backup failed on $(hostname)" | mail -s "Error Notification" -a "From: example@example.com" example@example.com
                 exit 1
 fi
 
@@ -45,13 +47,15 @@ fi
 rsync -av "$data_source" "$backup_dir/data/"
 if [ $? -ne 0 ]; then
             echo "Error: Failed to backup data folder."
+            echo "Nextcloud data backup failed on $(hostname)" | mail -s "Error Notification" -a "From: example@example.com" example@example.com
                 exit 1
 fi
 
 # Backup the database using mysqldump
-docker exec -t your-mysql mysqldump -u root -pPasswordHere NameofDatabase > "$backup_dir/nextcloud.sql"
+docker exec -t nextmysql mysqldump -u root -pYour-Password-Here Database_Name > "$backup_dir/nextcloud.sql"
 if [ $? -ne 0 ]; then
             echo "Error: Failed to backup the database."
+            echo "Nextcloud database backup failed on $(hostname)" | mail -s "Error Notification" -a "From: example@example.com" example@example.com
                 exit 1
 fi
 
@@ -65,8 +69,8 @@ echo "Backup completed successfully. Files and database are stored in $backup_di
 
 if [ $? -eq 0 ]; then
 
-        echo "Nextcloud backup to Synology completed successfully on $(hostname)" | mail -s "Success Notification" -a "From: your-email@example.com" your-email@example.com
+        echo "Nextcloud backup to Synology completed successfully maintenance mode OFF on $(hostname)" | mail -s "Success Notification" -a "From: example@scrtc.com" email@example.com
                 else
-        echo "Nextcloud backup encountered an error on $(hostname)" | mail -s "Error Notification" -a "From: your-email@example.com" your-email@example.com
+        echo "Nextcloud backup failed to automatically turn off maintenance mode on $(hostname)" | mail -s "Error Notification" -a "From: email@example.com" email@example.com
 
 fi
